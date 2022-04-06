06aabf8a10396f275b393f909882b886b4d11365
Adam <adamyusuf.ay@gmail.com>
2022-04-06 14:42:47 +0100

    First commit

---
+++ config/author.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Company/Author/Developer/Licence information of the application
+    |--------------------------------------------------------------------------
+    |
+    | All details about the author/developer/contact/licence of the application
+    |
+    | IMPORTANT: CHANGING ANY OF THIS INFORMATION WILL UNSTABILIZE THE APPLICATION
+    |
+    */
+
+    'vendor' => 'NEW-POS',
+    'vendor_url' => 'http://example.com',
+    'email' => 'example@example.com',
+    'app_version' => "8.0"
+];
